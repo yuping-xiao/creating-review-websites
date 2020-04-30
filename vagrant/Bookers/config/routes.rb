82a85c4854a3508b9c15453c90680678ad55b971
yuping-xiao <yupingxiao94@gmail.com>
2020-04-30 16:39:16 +0900

    almost done

--- vagrant/Bookers/config/routes.rb
+++ vagrant/Bookers/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'books#top'
+
+  get 'books' => 'books#index'
+
+  post 'books' => 'books#create'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
+  get 'books/:id' => 'books#show', as: 'book'
+
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
+  delete 'books/:id' => 'books#destroy' as: 'destroy_book'
+
 end
